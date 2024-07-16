89c387728a1ebb0a687705f3e91f68ec6bcabc4d
remystreich <remystreich@gmail.com>
2024-07-16 13:09:57 +0000

    relations

--- src/Entity/Book.php
+++ src/Entity/Book.php
@@ -10,24 +10,33 @@ use ApiPlatform\Metadata\ApiResource;
 use App\Repository\BookRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Doctrine\Orm\Filter\OrderFilter;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: BookRepository::class)]
 #[ApiResource]
-#[ApiResource(paginationClientItemsPerPage: true)]
+#[ApiResource(
+    normalizationContext: ['groups' => ['book:read']],
+    denormalizationContext: ['groups' => ['book:write']],
+    paginationClientItemsPerPage: true,
+)]
 #[ApiFilter(SearchFilter::class, properties: ['title' => 'partial'])]
 #[ApiFilter(OrderFilter::class, properties: ['title'])]
 class Book
 {
+    #[ApiProperty(identifier: true)]
+    #[Groups(['book:read'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Groups(['book:read', 'book:write'])]
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     private ?string $title = null;
 
+    #[Groups(['book:read', 'book:write'])]
     #[ORM\Column(length: 255)]
     #[Assert\NotBlank]
     #[Assert\Length(min: 3)]
@@ -35,10 +44,15 @@ class Book
     #[Assert\Length(min: 3, minMessage : "Le nom de l'auteur doit contenir au moins 3 caractères")]
     private ?string $author = null;
 
+    #[Groups(['book:read', 'book:write'])]
     #[ORM\Column(length: 255, nullable: true)]
     #[Assert\Length(exactly: 4)]
     #[Assert\NotBlank(groups: ["creation"])]
     private ?string $year = null;
+
+    #[Groups(['book:read', 'book:write'])]
+    #[ORM\ManyToOne(inversedBy: 'books')]
+    private ?Category $category = null;
 
 
     public function getId(): ?int
@@ -78,6 +92,18 @@ class Book
     public function setYear(string $year): static
     {
         $this->year = $year;
+
+        return $this;
+    }
+
+    public function getCategory(): ?Category
+    {
+        return $this->category;
+    }
+
+    public function setCategory(?Category $category): static
+    {
+        $this->category = $category;
 
         return $this;
     }
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -2,19 +2,88 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Metadata\ApiResource;
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
-
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Component\Serializer\Annotation\Groups;
+#[ApiResource(
+    normalizationContext: [ 'groups' => ['category']],
+    denormalizationContext: [ 'groups' => ['category:write']],
+)]
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
+#[ApiResource]
 class Category
 {
+    #[Groups(['category'])]
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
     private ?int $id = null;
 
+    #[Assert\NotBlank()]
+    #[Groups(['category', 'book', 'category:write'])]
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    /**
+     * @var Collection<int, Book>
+     */
+    #[Groups(['category'])]
+    #[ORM\OneToMany(targetEntity: Book::class, mappedBy: 'category')]
+    private Collection $books;
+
+    public function __construct()
+    {
+        $this->books = new ArrayCollection();
+    }
+
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Book>
+     */
+    public function getBooks(): Collection
+    {
+        return $this->books;
+    }
+
+    public function addBook(Book $book): static
+    {
+        if (!$this->books->contains($book)) {
+            $this->books->add($book);
+            $book->setCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeBook(Book $book): static
+    {
+        if ($this->books->removeElement($book)) {
+            // set the owning side to null (unless already changed)
+            if ($book->getCategory() === $this) {
+                $book->setCategory(null);
+            }
+        }
+
+        return $this;
     }
 }
